f3ba2910d5311e40b054a8fb85f2bb3e81a905f0
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-01-16 12:26:03 -0500

    Submit meals project

---
+++ arrays-in-objects.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const foo = {
+  meals: [
+    'breakfast',
+    'second breakfast',
+    'elevenses',
+    'lunch',
+    'afternoon tea',
+    'dinner',
+    'supper'
+  ],
+};
+
+console.log(foo.meals[3]);
